1e9d8b243550fa44c69f846949e2c3247f350f4c
Matheus Bordin <mabordin98@gmail.com>
2017-05-28 13:22:46 -0300

    Exclude js files

--- src/controller/home-controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const kamboja_1 = require("kamboja");
-class HomeController extends kamboja_1.Controller {
-    index() {
-        return this.view({ title: "Kamboja" });
-    }
-}
-exports.HomeController = HomeController;
